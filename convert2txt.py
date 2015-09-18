a140f5acc5b12439654d2e9a4a7644e1a93c2827
Sim Kennedy <sim__k@hotmail.com>
2015-09-18 13:36:00 +0100

    convert2txt ready to package

--- convert2txt.py
+++ convert2txt.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import os
-import errno
-from time import sleep
 import sys
-import subprocess
-from subprocess import Popen, PIPE
+import errno
 import shutil
 import csv
 import xlrd
 import docx
-import pdfminer
-import slate
-
+from subprocess import Popen, PIPE
 
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
@@ -25,7 +20,7 @@ def convert_pdf_to_txt(path):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
     codec = 'utf-8'
-    laparams = LAParams()
+    laparams = None
     device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
     fp = file(path, 'rb')
     interpreter = PDFPageInterpreter(rsrcmgr, device)
@@ -42,7 +37,7 @@ def convert_pdf_to_txt(path):
     return str
 
 
-def doc2txt(filename):
+def docx2txt(filename):
     document = docx.Document(filename)
     docText = '\n'.join([
         paragraph.text.encode('utf-8') for paragraph in document.paragraphs
@@ -55,13 +50,11 @@ def xls_to_csv(src, dst):
     sheets = x.sheet_names()
     for sheet in sheets:
         savefile = os.path.splitext(dst)[0] + ' - ' + sheet + '.txt'
-        print savefile
         x1 = x.sheet_by_name(sheet)
         with open(savefile, 'wb') as out:
             writecsv = csv.writer(out, quoting=csv.QUOTE_ALL)
             for rownum in xrange(x1.nrows):
-                row = [unicode(i).encode( 'utf-8') for i in x1.row_values(rownum)]
-                print rownum, row
+                row = [unicode(i).encode('utf-8') for i in x1.row_values(rownum)]
                 out.write(' '.join(row) + '\n')
 
 
@@ -74,40 +67,64 @@ def mkdir_p(path):
         else: raise
 
 
-rootDir = '/home/sim/git/CMA/root/Tender1'
-convertedDir = os.path.abspath(rootDir) + '-converted'
+def main(srcDir):
+    srcDir = '/home/sim/git/convert2txt/root/Tender1'
+    #srcDir = '/home/sim/git/convert2txt/examples/test'
+    convertedDir = os.path.abspath(srcDir) + '-converted'
 
+    failed_conversion = []
 
-failed_conversion = []
+    if os.path.exists(convertedDir):
+        shutil.rmtree(convertedDir)
 
-if os.path.exists(convertedDir):
-    shutil.rmtree(convertedDir)
-
-for path, dirs, files in os.walk(rootDir):
-    for f in files:
-        src = os.path.join(path, f)
-        dst = src.replace(rootDir, convertedDir)
-        dst = os.path.splitext(dst)[0] + '.txt'
-        filetype = os.path.splitext(src)[1].lower()
-        if filetype == '.pdf':
-            print src, dst
-            mkdir_p(os.path.dirname(dst))
-            with open(dst, 'wb') as out:
-                out.write(convert_pdf_to_txt(src))
-        if filetype == '.docx':
-            print src, dst
-            with open(dst, 'wb') as out:
-                out.write(doc2txt(src))
-        if filetype in ['.xls', '.xlsx']:
-            print src, dst
+    for path, dirs, files in os.walk(srcDir):
+        for f in files:
+            src = os.path.join(path, f)
             try:
-                xls_to_csv(src, dst)
+                dst = src.replace(srcDir, convertedDir)
+                dst = os.path.splitext(dst)[0] + '.txt'
+                mkdir_p(os.path.dirname(dst))
+                filetype = os.path.splitext(src)[1].lower()
+                if filetype == '.doc':
+                    with open(dst, 'wb') as out:
+                        cmd = ['antiword', src]
+                        p = Popen(cmd, stdout=PIPE)
+                        stdout, stderr = p.communicate()
+                        text = stdout.decode('ascii', 'ignore')
+                        text = unicode(text).encode('utf-8')
+                        out.write(text)
+                        if not os.path.isfile(dst):
+                            raise
+                if filetype == '.pdf':
+                    print 'Converting:', src
+                    with open(dst, 'wb') as out:
+                        out.write(convert_pdf_to_txt(src))
+                if filetype == '.docx':
+                    print 'Converting:', src
+                    with open(dst, 'wb') as out:
+                        out.write(docx2txt(src))
+                if filetype in ['.xls', '.xlsx']:
+                    print 'Converting:', src
+                    xls_to_csv(src, dst)
             except:
                 failed_conversion.append(src)
 
-print 'Failed\n', failed_conversion
+    print failed_conversion
+    if failed_conversion:
+        print 'Failed:\n', '\n'.join(failed_conversion)
+        failedFile = os.path.abspath(os.path.join(srcDir, os.pardir, 'failed_to_convert.txt'))
 
-failedFile = os.path.join(rootDir, '../failedConversions.txt')
-with open(failedFile, 'wb') as out:
-    for item in failed_conversion:
-        out.write(item)
+        with open(failedFile, 'wb') as out:
+            for item in failed_conversion:
+                out.write(item)
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    if len(args) < 2:  # need script
+        print 'Provide path to directory to convert.'
+        sys.exit(1)
+
+    path = args[1]
+
+    main(path)
--- setup.py
+++ setup.py
@@ -5,10 +5,4 @@ setup(
     version='1.0',
     description='The Spend Network conversion tool.',
     packages=find_packages(),
-    #package_dir = {'': 'src'},
-    install_requires=[],
-    extras_require={},
-    package_data={},
-    data_files=[],
-    entry_points={},
 )
